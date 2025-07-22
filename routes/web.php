bf2a15f8abfcdcf08687d5166510bb302cfd9216
Artur Yeszhanov <arturyeszhanov@gmail.com>
2025-07-23 00:36:02 +0500

    Define web routes for records

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecordController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('records', RecordController::class);
