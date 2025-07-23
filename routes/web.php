68fe7d9d7dd7b931da8be308a7e4f91b4414c824
Artur Yeszhanov <arturyeszhanov@gmail.com>
2025-07-23 14:58:16 +0500

    Update routes for record management

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RecordController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
+Route::post('/records/generate', [RecordController::class, 'generate'])->name('records.generate');
+Route::post('/records/clear', [RecordController::class, 'clear'])->name('records.clear');
+
 Route::resource('records', RecordController::class);
+Route::post('/records/setSheetUrl', [RecordController::class, 'setSheetUrl'])->name('records.setSheetUrl');
