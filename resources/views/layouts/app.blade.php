7a5447a3243a5d77474b9c432259fdd3035c8857
Artur Yeszhanov <arturyeszhanov@gmail.com>
2025-07-23 14:57:59 +0500

    Update layout: small changes in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Records</title>  
+    <title>Записи</title>  
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    @if(session('success'))
-        <p style="color: green">{{ session('success') }}</p>
-    @endif
-
     @yield('content')
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    @stack('scripts')
 </body>
 </html>
